44bf05655f1d36c403947c52e488e9a4d545a4d5
hyeon <hyon5807@gmail.com>
2026-03-05 21:55:54 +0900

    Spring 실습1 완료

---
+++ src/main/java/mylab/order/di/xml/config.java
@@ -0,0 +1,7 @@
+package mylab.order.di.xml;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class config {
+}
---
+++ src/test/java/mylab/order/di/xml/OrderSpringTest.java
@@ -0,0 +1,37 @@
+package mylab.order.di.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig
+@ContextConfiguration(locations="classpath:mylab-order-di.xml")
+public class OrderSpringTest {
+
+    @Autowired
+    ShoppingCart cart;
+
+    @Autowired
+    OrderService service;
+
+    @Test
+    public void testShoppingCart() {
+        assertNotNull(cart);
+
+        assertEquals(2, cart.getProducts().size());
+        assertEquals("노트북", cart.getProducts().get(0).getName());
+        assertEquals("스마트폰", cart.getProducts().get(1).getName());
+    }
+
+    @Test
+    public void testOrderService() {
+        assertNotNull(service);
+
+        double total = service.calculateOrderTotal();
+
+        assertEquals(2300000, total);
+    }
+}
